f1a8a9263ba5621bc474c67775944fd68d765580
Ramon Fabrega <ramonfabrega0@gmail.com>
2023-11-16 03:02:50 -0500

    init

---
+++ index.ts
@@ -0,0 +1,43 @@
+type TasksResponse = {
+  username: string;
+  league_tasks: number[];
+};
+
+(async () => {
+  const rmn = await fetchCompletedTasks("rmn69");
+  const capo = await fetchCompletedTasks("elcapo42069");
+
+  const compared = compareTasks({ rmn, capo });
+
+  console.log(compared);
+})();
+
+function compareTasks({
+  rmn,
+  capo,
+}: {
+  rmn: TasksResponse;
+  capo: TasksResponse;
+}) {
+  // Find tasks that are unique to rmn
+  const uniqueToRmn = rmn.league_tasks.filter(
+    (task) => !capo.league_tasks.includes(task)
+  );
+
+  // Find tasks that are unique to capo
+  const uniqueToCapo = capo.league_tasks.filter(
+    (task) => !rmn.league_tasks.includes(task)
+  );
+
+  return { rmn: uniqueToRmn, capo: uniqueToCapo };
+}
+
+async function fetchCompletedTasks(rsn: string) {
+  const res = await fetch(
+    `https://sync.runescape.wiki/runelite/player/${rsn}/TRAILBLAZER_RELOADED_LEAGUE`
+  );
+
+  const { username, league_tasks } = (await res.json()) as TasksResponse;
+
+  return { username, league_tasks };
+}
