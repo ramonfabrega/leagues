dc84fe433c9c86425aa4aed81786b392c36c0420
Ramon Fabrega <ramonfabrega0@gmail.com>
2023-12-13 00:43:47 -0500

    new accs

--- index.ts
+++ index.ts
@@ -5,52 +5,65 @@ type TasksResponse = {
   league_tasks: string[];
 };
 
-let cache: ReturnType<typeof compareTasks>;
+type ComparedTasks = ReturnType<typeof compareTasks>;
 
-(async () => {
-  while (true) {
-    await run();
-    await sleep(10000);
-  }
-})();
+let cache: ComparedTasks;
+
+while (true) {
+  await run();
+  await Bun.sleep(10000);
+}
 
 async function run() {
-  const rmn = await fetchCompletedTasks("rmn69");
-  const capo = await fetchCompletedTasks("elcapo42069");
+  const rmn = await fetchCompletedTasks("rmn420");
+  const capo = await fetchCompletedTasks("elcapo69420");
 
   const compared = compareTasks({ rmn, capo });
 
   if (!Bun.deepEquals(cache, compared)) {
-    if (cache) {
-      // Log new tasks for rmn
-      const newTasksForRmn = compared.rmn.filter(
-        (task) => !cache.rmn.includes(task)
-      );
-      if (newTasksForRmn.length > 0) {
-        console.log("New tasks for rmn:", newTasksForRmn);
-      }
-
-      // Log new tasks for capo
-      const newTasksForCapo = compared.capo.filter(
-        (task) => !cache.capo.includes(task)
-      );
-      if (newTasksForCapo.length > 0) {
-        console.log("New tasks for capo:", newTasksForCapo);
-      }
-    }
-    // log the difference between cache and compared
+    logNewTasks(compared);
     cache = compared;
     console.log(compared);
   }
 }
 
+function logNewTasks(compared: ComparedTasks) {
+  if (cache) {
+    const newTasksForRmn = compared.rmn.filter(
+      (task) => !cache.rmn.includes(task)
+    );
+    const nowCommonTasksForRmn = cache.rmn.filter((task) =>
+      compared.capo.includes(task)
+    );
+
+    if (newTasksForRmn.length > 0) {
+      console.log("New tasks for rmn:", newTasksForRmn);
+    }
+    if (nowCommonTasksForRmn.length > 0) {
+      console.log("Tasks now common for rmn:", nowCommonTasksForRmn);
+    }
+
+    const newTasksForCapo = compared.capo.filter(
+      (task) => !cache.capo.includes(task)
+    );
+    const nowCommonTasksForCapo = cache.capo.filter((task) =>
+      compared.rmn.includes(task)
+    );
+
+    if (newTasksForCapo.length > 0) {
+      console.log("New tasks for capo:", newTasksForCapo);
+    }
+    if (nowCommonTasksForCapo.length > 0) {
+      console.log("Tasks now common for capo:", nowCommonTasksForCapo);
+    }
+  }
+}
+
 function compareTasks({ rmn, capo }: Record<"rmn" | "capo", TasksResponse>) {
-  // Find tasks that are unique to rmn
   const uniqueToRmn = rmn.league_tasks.filter(
     (task) => !capo.league_tasks.includes(task)
   );
 
-  // Find tasks that are unique to capo
   const uniqueToCapo = capo.league_tasks.filter(
     (task) => !rmn.league_tasks.includes(task)
   );
@@ -70,8 +83,4 @@ async function fetchCompletedTasks(rsn: string) {
   );
 
   return { username, league_tasks: tasks };
-}
-
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
